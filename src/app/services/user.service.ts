f7e96eaf2ae240d194ce5bfc07321272da2f0e53
Nikolay Kochetov <nikochetov@gmail.com>
2020-07-26 00:27:41 +0300

    add remove user from datatable

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import {User} from '../user';
 import {MatTableDataSource} from '@angular/material/table';
-import {filter} from "rxjs/operators";
-
-
 
 @Injectable({providedIn: 'root'})
 export class UserService {
