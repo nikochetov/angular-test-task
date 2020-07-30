589bfd80fdcdb3607b3baa268932d1716f92fdb9
Nikolay Kochetov <nikochetov@gmail.com>
2020-07-30 10:11:56 +0300

    fix bugs and format code

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { User } from '../user';
 import { MatTableDataSource } from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
 
 @Injectable({providedIn: 'root'})
 export class UserService {
   user: User;
   USERS_DATA: User[] = [];
   dataSource = new MatTableDataSource(this.USERS_DATA);
+  selection = new SelectionModel<User>(true, []);
+
   addUser(user): void {
     this.USERS_DATA.push(user);
     this.dataSource = new MatTableDataSource(this.USERS_DATA);
+  }
+
+  remove(): void {
+    const ids: number[] = this.selection.selected.map(user => user.id);
+    this.USERS_DATA = this.USERS_DATA.filter((user: User) => !ids.includes(user.id));
+    this.dataSource = new MatTableDataSource(this.USERS_DATA);
+    this.selection.clear();
   }
 
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -14,34 +14,29 @@ export class TableComponent implements OnInit{
   constructor(public userService: UserService) {
   }
   displayedColumns: string[] = ['select', 'userName', 'birth', 'gender', 'snils'];
-  selection = new SelectionModel<User>(true, []);
 
   ngOnInit(): void {
   }
+
   isAllSelected(): any {
-    const numSelected = this.selection.selected.length;
+    const numSelected = this.userService.selection.selected.length;
     const numRows = this.userService.dataSource.data.length;
     return numSelected === numRows;
   }
 
   masterToggle(): void {
     this.isAllSelected() ?
-      this.selection.clear() :
-      this.userService.dataSource.data.forEach(row => this.selection.select(row));
+      this.userService.selection.clear() :
+      this.userService.dataSource.data.forEach(row => this.userService.selection.select(row));
   }
 
   checkboxLabel(row?: User): string {
     if (!row) {
       return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id}`;
+    return `${this.userService.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id}`;
   }
 
-  remove(): void {
-    const ids: number[] = this.selection.selected.map(user => user.id);
-    this.userService.USERS_DATA = this.userService.USERS_DATA.filter((user: User) => !ids.includes(user.id));
-    this.userService.dataSource = new MatTableDataSource(this.userService.USERS_DATA);
-    this.selection.clear();
-  }
+
 }
 
